e965b5f3e777f55d2d2b158b0377a2fd6e72aa49
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-08-18 01:17:39 -0400

    Built function to allow customer to choose the task they wish to complete -- buy something, view products list, or quit

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -5,12 +5,6 @@ var inquirer = require("inquirer");
 var Table = require("cli-table");
 var mysql = require("mysql");
 
-var table = new Table(
-{
-    head: ["Product ID", "Product Name", "Department", "Price", "Avail Qty"],
-    colWidths: [15, 50, 50, 15, 15]
-});
-
 // Creates MySQL Connection
 var connection = mysql.createConnection(
 {
@@ -32,16 +26,21 @@ var connection = mysql.createConnection(
 // This is a function that is call to list all of the products on Bamazon
 function listProducts()
 {
-    console.log("Retrieving all products from Bamazon...\n");
+    console.log("\nRetrieving all products from Bamazon...\n");
 
     connection.query("SELECT * FROM products", function(err, res)
     {
         // If an error is encountered, then the error is displayed to the customer
         if (err) throw err;
 
+        var productsTable = new Table(
+            {
+                head: ["Product ID", "Product Name", "Department", "Price", "Avail Qty"],
+                colWidths: [15, 50, 50, 15, 15]
+            });
+
         var product = [];
-        var productList = [];
-        // Loops through MySQL query results and displays each product
+
         for (i = 0; i < res.length; i++)
         {
             product.push(res[i].item_id);
@@ -50,20 +49,73 @@ function listProducts()
             product.push(res[i].price);
             product.push(res[i].stock_qty);
 
-            table.push(product);
+            productsTable.push(product);
 
             product = [];
         }
 
-        console.log(table.toString());
-        //console.log(productList);
+        console.log(productsTable.toString()+ "\n");
+
+        pickTask();
+
     });
+}
+
+function pickTask()
+{
+    var task;
+    
+    inquirer.prompt(
+    [
+        {
+            type: "list",
+            name: "task",
+            message: "What would you like to do?",
+            choices:
+            [
+                "Make a Purchase",
+                "List Products",
+                "Quit Bamazon"
+            ]
+        }
+    ]).then(function(response)
+    {
+        task = response.task;
+
+        // Switch that invokes the function associated with the task the user wishes to perform
+        switch(task)
+        {
+            // Invokes the buyProduct function
+            case "Make a Purchase":
+                buyProduct();
+                break;
+
+            // Invokes the buyProduct function
+            case "List Products":
+                listProducts();
+                break;
+
+            // Quits Bamazon
+            case "Quit Bamazon":
+                process.exit();
+                break;
+        }
+    });
+}
+
+function buyProduct()
+{
+    console.log("\nLet's buy something!\n")
+    pickTask();
 }
 
 connection.connect(function(err)
 {
     if (err) throw err;
     
-    console.log("You are connected as: " + connection.threadId + "\n");
+    console.log("\nYou are connected as: User" + connection.threadId + "\n");
+
+    console.log("Welcome to Bamazon! Please take a look at all we have to sell. Happy shopping!");
+
     listProducts();
 });
